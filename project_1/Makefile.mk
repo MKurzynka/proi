fbc4723a2125633b994cefa8d32e4e1a5a863718
MacSzu <37077729+MacSzu@users.noreply.github.com>
2018-03-14 21:02:28 +0100

    p1 makefile

--- project_1/Makefile.mk
+++ project_1/Makefile.mk
@@ -1,11 +1,11 @@
-bin/app : build/app.o
-	g++ build/app.o build/HashArray.o build/HashArrayUI.o -o bin/app.out
+bin/HashArrayApp : build/HashArrayApp.o build/
+	g++ -std=c++0x build/app.o build/HashArray.o build/HashArrayUI.o -o bin/app.out
 
-build/app.o : src/HashArrayApp.cpp
-	g++ -c src/HashArrayApp.cpp -o -Iinclude build/app.o
+build/HashArrayApp.o : src/HashArrayApp.cpp include/HashArrayUI.hpp include/HashArray.hpp
+	g++ -c src/HashArrayApp.cpp -o  build/HashArrayApp.o
 
-build/HashArray.o : src/HashArray.cpp
-	g++ -c src/HashArray.cpp -o -Iinclude build/HashArray.o
+build/HashArray.o : src/HashArray.cpp include/HashArray.hpp
+	g++ -c src/HashArray.cpp -o build/HashArray.o
 
-build/HashArrayUI.o : src/HashArrayUI.cpp
-	g++ -c src/HashArrayUI.cpp -o -Iinclude build/HashArrayUI.o
+build/HashArrayUI.o : src/HashArrayUI.cpp include/HashArray.hpp include/HashArrayUI.hpp
+	g++ -c src/HashArrayUI.cpp -o build/HashArrayUI.o
