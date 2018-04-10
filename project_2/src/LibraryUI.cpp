39bfc3bdb7e84db547cdeb0c6bf4f363a5ec8ec6
MacSzu <maciekszu@tlen.pl>
2018-04-10 23:54:05 +0200

    xd

--- project_2/src/LibraryUI.cpp
+++ project_2/src/LibraryUI.cpp
@@ -13,11 +13,11 @@
 
 using namespace std;
 
-LibraryUI::LibraryUI(Library* lib) : library_(*lib) {};
+LibraryUI::LibraryUI(Library* lib) : library_(lib) {};
 
 LibraryUI::~LibraryUI() {
   cout<<"Usuwam"<<endl;
-  delete &library_;
+  delete library_;
 }
 void LibraryUI::run() {
     int action_choice = 0;
@@ -60,14 +60,14 @@ void LibraryUI::run() {
 }
 
 void LibraryUI::show() {
-    library_.showItems();
+    library->showItems();
 }
 
 void LibraryUI::showByName() {
     string name;
     cout<<"Type in a name: ";
     cin>>name;
-    library_.showItems(name);
+    library->showItems(name);
 }
 
 void LibraryUI::addItem() {
@@ -85,9 +85,9 @@ void LibraryUI::addItem() {
     cout<<"Number of pages: ";
     cin>>num_of_pages;
     if(type) {
-        library_.addItem<unsigned short int>(name, author, num_of_copies ,num_of_pages);
+        library->addItem<unsigned short int>(name, author, num_of_copies ,num_of_pages);
     } else {
-        library_.addItem<uint8_t>(name, author, num_of_copies, num_of_pages);
+        library->addItem<uint8_t>(name, author, num_of_copies, num_of_pages);
     }
 }
 
@@ -95,7 +95,7 @@ void LibraryUI::removeItem() {
     int id;
     cout<<"Id: ";
     cin>>id;
-    library_.removeItem(id);
+    library->removeItem(id);
 }
 
 void LibraryUI::increaseCopies() {
@@ -104,7 +104,7 @@ void LibraryUI::increaseCopies() {
     cin>>id;
     cout<<"Increase the number of copies by: ";
     cin>>n;
-    library_.increaseCopies(id,n);
+    library->increaseCopies(id,n);
 }
 
 void LibraryUI::decreaseCopies() {
@@ -113,5 +113,5 @@ void LibraryUI::decreaseCopies() {
     cin>>id;
     cout<<"Decrease the number of copies by: ";
     cin>>n;
-    library_.decreaseCopies(id,n);
+    library->decreaseCopies(id,n);
 }
