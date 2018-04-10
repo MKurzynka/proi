a9562521e4559c589f22ad9ef78146c85b62ad2d
MacSzu <maciekszu@tlen.pl>
2018-04-10 19:27:45 +0200

    ui change

--- project_2/include/Item.h
+++ project_2/include/Item.h
@@ -18,8 +18,9 @@ template <typename T> class Item : public ItemInterface {
         Item(int id, std::string name, std::string author, int num_of_copies, T num_of_pages) : ItemInterface(id, name, author, num_of_copies), num_of_pages_(num_of_pages) {};
         ~Item() {};
         void show(std::ostream& stream) const {
-            stream<<"________________________________________________\n";
+            stream<<"____________________________________________________________________________________\n";
             stream<<"ID: "<<id_<<"\nname: "<<name_<<"\nauthor: "<<author_<<"\nNumber of copies: "<<num_of_copies_<<"\nNumber of pages: "<<(int)num_of_pages_<<'\n';
+            stream<<"____________________________________________________________________________________\n";
         }
 };
 #endif // ITEM_H
